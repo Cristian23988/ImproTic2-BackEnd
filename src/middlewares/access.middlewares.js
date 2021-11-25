6bd8be05898884a1bf5270a6d54941afa32e5d16
Cristian23988 <49573338+Cristian2398@users.noreply.github.com>
2021-11-25 18:59:31 -0500

    ya casi

--- src/middlewares/access.middlewares.js
+++ src/middlewares/access.middlewares.js
@@ -1,5 +1,22 @@
-const validateAccess = () => {
-  console.log('Validating access');
+// vendors
+import jwt from 'jsonwebtoken';
+
+// models
+import Users from '../models/users.model.js';
+
+const validateAuthentication = async (req) => {
+  const token = req.headers.authorization;
+  try {
+    const session = await jwt.verify(token, process.env.SECRET);
+    const user = await Users.findById(session.userId);
+    return {
+      user,
+    }
+  } catch (error) {
+    return {
+      errorMessage: error.message,
+    }
+  }
 };
 
-export default validateAccess;
+export default validateAuthentication;
